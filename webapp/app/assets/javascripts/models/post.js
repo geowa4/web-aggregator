aa60a6c8a4c8992ba1dba4ba9d10850396f56cf4
George Adams <g.w.adams.iv@gmail.com>
2012-01-08 12:10:24 -0500

    i dont like it but parsing xml works

--- webapp/app/assets/javascripts/models/post.js
+++ webapp/app/assets/javascripts/models/post.js
@@ -5,12 +5,29 @@ $(function() {
 	var Post = Backbone.Model.extend({
 		url: function() {
 			return urlRoot + '/' + this.id + '.' + dataType;
+		},
+
+		parse: function(response) {
+			var $xml = $(response);
+			var posts = [];
+			$xml.find('entry').each(function() {
+				var data = {};
+				var $entry = $(this);
+				data['id'] = $entry.find('id').text();
+				data['title'] = $entry.find('title').text();
+				data['updated'] = $entry.find('updated').text();
+				data['summary'] = $entry.find('summary').text();
+				posts.push(data)
+			});
+			return posts[0];
 		}
 	});
 
 	var Posts = Backbone.Collection.extend({
 		model: Post,
+
 		url: urlRoot + '.' + dataType,
+
 		parse: function(response) {
 			var $xml = $(response);
 			var posts = [];
@@ -26,22 +43,6 @@ $(function() {
 			return posts;
 		}
 	});
-
-	/*Backbone.sync = function(method, model, options) {
-		console.log(method);
-
-		var type = this.methodMap[method];
-		
-		// Default JSON-request options.
-		var params = _.extend({
-			type:         type,
-			contentType:  'application/xml',
-			dataType:     dataType,
-			processData:  false
-		}, options);
-
-		Backbone.prototype.sync.call(this, method, model, params);
-	}*/
 
 	// Expose Post objects
 	window.Post = Post;
