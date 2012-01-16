4e1aa3bed37dcb65c4472b16b390dcf76e6ec883
George Adams <g.w.adams.iv@gmail.com>
2012-01-15 21:05:46 -0500

    using the metadata column

--- webapp/app/assets/javascripts/views/post-list.js
+++ webapp/app/assets/javascripts/views/post-list.js
@@ -2,13 +2,13 @@
 	var postListTemplate = _.template(
 		'<article class="post-snippet" data-id="<%= post.id %>">' + 
 			'<header>' + 
-			'<h4><%= post.get("title") %></h4>' + 
-			'<h5><time pubdate datetime="<%= post.get("updated") %>"><%= post.get("updated") %></time></h5>' + 
+			'<h4><%= post.get("title") %></h4>' +
 			'</header>' + 
 			'<p><%= post.get("content") %></p>' +
-			'<footer><a href="<%= post.get("url") %>"><small>Original Post</small></a></footer>' +
 			'</article>'
 	);
+	var publishDateTemplate = _.template('<time pubdate datetime="<%= post.get("published") %>"><%= post.get("published") %></time>');
+	var updateDateTemplate = _.template('<time datetime="<%= post.get("updated") %>"><%= post.get("updated") %></time>');
 
 	/**
 	   This widget relies on Underscore and 
@@ -31,6 +31,11 @@
 		},
 
 		_create: function() {
+			this.postList = this.element.find('.post-list');
+			this.metaTable = this.element.find('.post-meta table');
+			this.sourceLink = this.metaTable.find('.source-link a');
+			this.publishDate = this.metaTable.find('.publish-date');
+			this.updateDate = this.metaTable.find('.update-date');
 			this.refresh();
 		},
 
@@ -39,15 +44,20 @@
 			var self = this;
 			if(this.options.posts) {
 				this.options.posts.each(function(post) {
-					var $postItem = self.items[post.id];
-					if($postItem === undefined) {
-						$postItem = $(postListTemplate({post: post}));
-						self.element.append($postItem);
-						self.items[post.id] = $postItem;
+					var postItem = self.items[post.id];
+					if(postItem === undefined) {
+						postItem = $(postListTemplate({post: post}));
+						self.postList.append(postItem);
+						self.items[post.id] = postItem;
+						self.sourceLink.attr('href', post.get('url'));
+						self.publishDate.html(publishDateTemplate({post: post}));
+						self.updateDate.html(updateDateTemplate({post: post}));
 					} else {
-						$postItem.find('h3').text(post.get('title'));
-						$postItem.find('p').html(post.get('content'));
-						$postItem.find('footer a').attr('href', post.get('url'));
+						postItem.find('h3').text(post.get('title'));
+						postItem.find('p').html(post.get('content'));
+						self.sourceLink.attr('href', post.get('url'));
+						self.publishDate.html(publishDateTemplate({post: Post}));
+						self.updateDate.html(updateDateTemplate({post: Post}));
 					}
 				});
 			}
