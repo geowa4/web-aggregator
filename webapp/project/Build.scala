9bf92c3dba1fc46ebd561e551bd8ed8e9993b534
George Adams <g.w.adams.iv@gmail.com>
2012-02-13 20:51:39 -0500

    needed to play with dependencies to ensure that i could get a build. stupid nightlies.

--- webapp/project/Build.scala
+++ webapp/project/Build.scala
@@ -8,14 +8,15 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0"
 
     val appDependencies = Seq(
-	  "org.joda" % "joda-convert" % "1.2",
+	  "maven-plugins" % "maven-cobertura-plugin" % "1.4",
+	  "maven-plugins" % "maven-findbugs-plugin" % "1.4",
 	  "org.apache.abdera" % "abdera-parser" % "1.1.1",
+	  "org.joda" % "joda-convert" % "1.2",
 	  "joda-time" % "joda-time" % "2.0",
-	  "net.liftweb"    %% "lift-mongodb-record" % "2.4-RC1",
-	  "com.foursquare" %% "rogue"               % "1.0.29" intransitive()
+	  "net.liftweb"    %% "lift-mongodb-record" % "2.4",
+	  "com.foursquare" %% "rogue"               % "1.1.2" intransitive()
 	)
 
-    val main = PlayProject(appName, appVersion, appDependencies, 
-						   mainLang = SCALA)
+    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA)
 
 }
