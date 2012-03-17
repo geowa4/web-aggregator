393e749296dcd93d8943b843f2c6813470bc67d0
George Adams <g.w.adams.iv@gmail.com>
2012-03-16 23:00:28 -0400

    formatting

--- webapp/app/Global.scala
+++ webapp/app/Global.scala
@@ -4,23 +4,23 @@ import models._
 import sync._
 
 class Global extends GlobalSettings {
- 
+
   override def onStart(app: Application) {
-	PostMongo.connectToMongo
-	GooglePlusSync.start
-	GitHubSync.start
-	StackOverflowSync.start
-	BloggerSync.start
-	Logger.info("Application has started.");
+    PostMongo.connectToMongo
+    GooglePlusSync.start
+    GitHubSync.start
+    StackOverflowSync.start
+    BloggerSync.start
+    Logger.info("Application has started.");
   }  
-  
+
   override def onStop(app: Application) {
-	GooglePlusSync.stop
-	GitHubSync.stop
-	StackOverflowSync.stop
-	BloggerSync.stop
-	PostMongo.disconnectFromMongo
-	Logger.info("Application has shutdown.");
+    GooglePlusSync.stop
+    GitHubSync.stop
+    StackOverflowSync.stop
+    BloggerSync.stop
+    PostMongo.disconnectFromMongo
+    Logger.info("Application has shutdown.");
   }  
-  
+
 }
