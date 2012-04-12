dc16a19715d26d2ed0fe7626ae35ba605168771b
George Adams <g.w.adams.iv@gmail.com>
2012-04-11 21:53:25 -0400

    Starting the switch to CoffeeScript.

---
+++ webapp/app/assets/javascripts/models/postModel.coffee
@@ -0,0 +1,27 @@
+$(() ->
+  urlRoot = '/posts'
+  dataType = 'json'
+
+  Post = Backbone.Model.extend
+    defaults:
+      url: ""
+      title: ""
+      content: ""
+      published: new Date
+      updated: new Date
+      provider: ""
+
+    initialize: () ->
+      this.set
+        published: new Date(this.get('published'))
+        updated: new Date(this.get('updated'))
+
+    url: () -> urlRoot + '/' + this.id + '.' + dataType
+
+  Posts = Backbone.Collection.extend
+    model: Post
+
+    url: urlRoot + '.' + dataType
+
+  window.Posts = Posts
+)
--- webapp/project/Build.scala
+++ webapp/project/Build.scala
@@ -4,19 +4,20 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "web-aggregator"
-    val appVersion      = "1.0"
+  val appName = "web-aggregator"
+  val appVersion = "1.0"
 
-    val appDependencies = Seq(
-	  "maven-plugins" % "maven-cobertura-plugin" % "1.4",
-	  "maven-plugins" % "maven-findbugs-plugin" % "1.4",
-	  "org.apache.abdera" % "abdera-parser" % "1.1.1",
-	  "org.joda" % "joda-convert" % "1.2",
-	  "joda-time" % "joda-time" % "2.0",
-	  "net.liftweb"    %% "lift-mongodb-record" % "2.4",
-	  "com.foursquare" %% "rogue"               % "1.1.2" intransitive()
-	)
+  val appDependencies = Seq(
+    "maven-plugins" % "maven-cobertura-plugin" % "1.4",
+    "maven-plugins" % "maven-findbugs-plugin" % "1.4",
+    "org.apache.abdera" % "abdera-parser" % "1.1.1",
+    "org.joda" % "joda-convert" % "1.2",
+    "joda-time" % "joda-time" % "2.0",
+    "net.liftweb" %% "lift-mongodb-record" % "2.4",
+    "com.foursquare" %% "rogue" % "1.1.2" intransitive()
+  )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA)
-
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    coffeescriptOptions := Seq("bare")
+  )
 }
